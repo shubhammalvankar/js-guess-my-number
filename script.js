a2003b18bad7459d63c352c3fd2e465f52c00b53
Shubham Malvankar <shubhammalvankar@Shubhams-MacBook-Pro.local>
2024-07-15 20:49:42 +0530

    Manipulate CSS style

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  // console.log(guess, typeof guess);
 
   if (!guess) {
+    // No input
     document.querySelector('.message').textContent = '⛔️ No Number!';
   } else if (guess === secretNumber) {
+    // Player wins
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
+    // Guess is high
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
       score--;
@@ -23,6 +30,8 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.score').textContent = score;
     }
   } else if (guess < secretNumber) {
+    // Guess is low
+
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
       score--;
